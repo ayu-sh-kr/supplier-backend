ab3a412d1b72bca97ee7ff240131e3cb0064c6d9
ayu-sh-kr <akjaiswal2003@gmail.com>
2023-12-08 05:29:46 +0530

    Represent the order made by the Buyer

--- src/main/java/dev/archimedes/enities/shipment/Order.java
+++ src/main/java/dev/archimedes/enities/shipment/Order.java
@@ -1,21 +1,37 @@
 package dev.archimedes.enities.shipment;
 
+import dev.archimedes.enities.supplier.Branch;
 import dev.archimedes.enities.supplier.Product;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity(name = "orders")
 @Table(name = "orders")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter @Setter
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "oder_id")
+    @Column(name = "order_id")
     private long id;
+
     private String orderStatus;
+
     @ManyToOne
+    @JoinColumn(name = "buyerId")
     private Buyer buyer;
-    @ManyToMany(mappedBy = "orders")
+
+    @ManyToOne
+    @JoinColumn(name = "branchId")
+    private Branch branch;
+
+    @ManyToMany(mappedBy = "orders", fetch = FetchType.LAZY)
     private List<Product> products = new ArrayList<>();
 }
