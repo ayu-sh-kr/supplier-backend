b6a26cc8c04caa54ef54fea0a07c96ccbf134fec
ayu-sh-kr <akjaiswal2003@gmail.com>
2023-12-08 05:40:59 +0530

    ENUM to represent the current state of order in transit

--- src/main/java/dev/archimedes/enums/OrderStatus.java
+++ src/main/java/dev/archimedes/enums/OrderStatus.java
@@ -1,5 +1,8 @@
 package dev.archimedes.enums;
 
 public enum OrderStatus {
-    PLACED, SHIPPED, DELIVERED, NEARBY, ACCEPTED
+    PLACED, SHIPPED, DELIVERED, NEARBY, ACCEPTED;
+    OrderStatus(){
+
+    }
 }
