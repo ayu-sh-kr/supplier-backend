bf21d0bae6df379194be5d3646248dd67e834ef3
ayu-sh-kr <akjaiswal2003@gmail.com>
2023-12-08 05:30:04 +0530

    Represent Buyer entity

--- src/main/java/dev/archimedes/enities/shipment/Buyer.java
+++ src/main/java/dev/archimedes/enities/shipment/Buyer.java
@@ -1,13 +1,22 @@
 package dev.archimedes.enities.shipment;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import dev.archimedes.enities.Address;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Entity(name = "buyers")
 @Table(name = "buyers")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter @Setter
 public class Buyer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,11 +25,16 @@ public class Buyer {
     private String name;
     private String email;
     private String password;
-    @OneToOne
-    private Cart cart;
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "buyer")
+    @JsonIgnore
+    private List<Cart> cart;
     @OneToOne
     private Address address;
-
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "buyer")
+    @JsonIgnore
     private Set<Order> orders = new HashSet<>();
+
+    public void addOrder(Order order) {
+        this.orders.add(order);
+    }
 }
