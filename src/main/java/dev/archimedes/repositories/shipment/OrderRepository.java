9ba22c59046834ca04b3e4f2b89455515806bb49
ayu-sh-kr <akjaiswal2003@gmail.com>
2023-12-08 05:38:38 +0530

    OrderRepository to extract data related to order from data base

--- src/main/java/dev/archimedes/repositories/shipment/OrderRepository.java
+++ src/main/java/dev/archimedes/repositories/shipment/OrderRepository.java
@@ -1,7 +1,15 @@
 package dev.archimedes.repositories.shipment;
 
+import dev.archimedes.enities.projections.OrderProjection;
 import dev.archimedes.enities.shipment.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    @Query("select o from orders o")
+    List<OrderProjection.OrderInfo> getAllOrders();
+
 }
